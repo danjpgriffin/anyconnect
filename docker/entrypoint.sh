dc4643ba9901a6e83d876ca31fbc89b784fd2541
Dan Griffin <github.mk@xiragon.com>
2020-06-08 14:49:43 +0200

    Always use DNS 8.8.8.8 inside the container to lookup initial VPN entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cp /etc/resolv.conf /etc/resolv.conf.bak
 umount /etc/resolv.conf
-cp /resolv.conf /etc/resolv.conf
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
 sysctl net.ipv4.conf.all.forwarding=1
-/fix-firewall.sh &
 
+/fix-firewall.sh &
 /start-traps.sh &
 
 /opt/cisco/anyconnect/bin/vpnagentd
