f13eb6be0540fe842ac198b490398ee817697e56
Josef Hoerandtner <josef.hoerandtner@springer.com>
2022-06-02 19:55:08 +0200

    Avoid iptables warning

--- docker/fix-firewall.sh
+++ docker/fix-firewall.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 keep_forwarding_clear() {
-	iptables -w -L FORWARD | grep ciscovpn > /dev/null
-	FOUND=$?
+  iptables -w -L FORWARD | grep ciscovpn &>/dev/null
+  FOUND=$?
 
 	if [ $FOUND -eq 0 ] ; then
-	    echo Flushing forwarding restrictions
-	    iptables -w -F FORWARD
-	    echo Setting up masquarade
-	    iptables -w -t nat -D POSTROUTING -o cscotun0 -j MASQUERADE > /dev/null
-	    iptables -w -t nat -A POSTROUTING -o cscotun0 -j MASQUERADE
-	fi
+    echo Flushing forwarding restrictions
+    iptables -w -F FORWARD
+    echo Setting up masquarade
+    # Delete rule if it already exists (multiple times?)
+    while iptables -w -t nat -D POSTROUTING -o cscotun0 -j MASQUERADE &>/dev/null; do true; done
+    # Add rule
+    iptables -w -t nat -A POSTROUTING -o cscotun0 -j MASQUERADE
+  fi
 }
 
 while true; do keep_forwarding_clear; sleep 1; done
